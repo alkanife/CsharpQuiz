3729c77d351d08b6c93abdc14b54cfd782b5640c
Arthur B. / Alkanife <alkanife@gmail.com>
2023-05-27 15:48:00 +0200

    Proof of concept

--- CsharpQuiz/Program.cs
+++ CsharpQuiz/Program.cs
@@ -9,10 +9,11 @@ namespace CsharpQuiz
         private static void Main(string[] args)
         {
             CreateQuestions();
-            
+
+            Console.Clear();
             Console.WriteLine("Bienvenue au Quiz C# !");
             Console.WriteLine($"Il y a {_questions.Count} questions");
-            Console.WriteLine("Êtes-vous prêt ? (entrer)");
+            Console.WriteLine("Taper entrer si vous êtes prêt");
             Console.ReadKey();
 
             var i = 0;
@@ -27,11 +28,72 @@ namespace CsharpQuiz
             
             Console.WriteLine("Vous avez répondu a toutes les questions !");
 
-            var score = _questions.Count(question => question.GoodResponse);
+            var score = _questions.Count(question => question.UserResponse == question.ValidResponse);
 
             Console.WriteLine($"Votre score: {score}/{_questions.Count}");
             
             Console.WriteLine("Voulez-vous avoir un récap approfondi ? (Y/n)");
+            
+            var userStringInput = Console.ReadLine();
+
+            if (userStringInput == null)
+            {
+                Console.WriteLine("Erreur console (null)");
+                return;
+            }
+                
+            if (userStringInput.ToLower().Equals("n"))
+            {
+                Console.WriteLine("Au revoir");
+                return;
+            }
+            
+            i = 0;
+            foreach (var question in _questions)
+            {
+                i++;
+                ShowRecap(question, i);
+            }
+            
+            Console.Clear();
+            Console.WriteLine($"Fin du récap ! Pour rappel votre score était de {score}/{_questions.Count}. Au revoir !");
+        }
+
+        private static void ShowRecap(Question question, int id)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("RECAPITULATIF");
+            Console.WriteLine($"Question {id} sur {_questions.Count}");
+            Console.WriteLine();
+            Console.WriteLine($"{question.Title}");
+            
+            var i = 1;
+            foreach (var resp in question.Responses!)
+            {
+                Console.Write(i == question.ValidResponse ? "[V] " : "[X] ");
+                
+                Console.Write($"{i}. {resp}");
+                
+                Console.Write(i == question.UserResponse ? " <-- Votre réponse" : "");
+                Console.Write("\n");
+                i++;
+            }
+            
+            Console.WriteLine();
+
+            if (question.UserResponse == question.ValidResponse)
+            {
+                Console.WriteLine("Vous avez trouvé la bonne réponse, bien joué !");
+            }
+            else
+            {
+                Console.WriteLine(question.Recap);
+            }
+            
+            
+            Console.WriteLine("\nTaper entrer pour continuer");
+            Console.ReadLine();
         }
 
         private static bool AskQuestion(Question question, int id)
@@ -41,8 +103,8 @@ namespace CsharpQuiz
             Console.WriteLine();
             Console.WriteLine($"{question.Title}");
 
-            int i = 1;
-            foreach (var resp in question.Responses)
+            var i = 1;
+            foreach (var resp in question.Responses!)
             {
                 Console.WriteLine($"{i}. {resp}");
                 i++;
@@ -76,11 +138,7 @@ namespace CsharpQuiz
                 }
                 else
                 {
-                    if (userIntInput == question.ValidResponse)
-                        question.GoodResponse = true;
-                    
-                    Console.WriteLine($"Réponse: {userIntInput}");
-                    Console.ReadLine();
+                    question.UserResponse = userIntInput;
                     break;
                 }
             } while (true);
@@ -108,7 +166,7 @@ namespace CsharpQuiz
                 new()
                 {
                     Title = "Question test",
-                    Responses = new string[4]
+                    Responses = new []
                     {
                         "Réponse de test (vraie)",
                         "Réponse de test (fausse)",
@@ -118,126 +176,16 @@ namespace CsharpQuiz
                     ValidResponse = 1,
                     Recap = "Récap de test !"
                 },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                },
-                new()
-                {
-                    Title = "Question test",
-                    Responses = new string[4]
-                    {
-                        "Réponse de test (vraie)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)",
-                        "Réponse de test (fausse)"
-                    },
-                    ValidResponse = 1,
-                    Recap = "Récap de test !"
-                }
             };
         }
     }
 
     internal class Question
     {
-        // Titre de la question
-        public string? Title { get; set; }
-        // Réponses possibles
-        public string[]? Responses { get; set; }
-        // La réponse valide
-        public int ValidResponse { get; set; }
-        // Le récap si la réponse n'est pas bonne 
-        public string? Recap { get; set; }
-
-        public bool GoodResponse { get; set; } = false;
-
+        public string? Title { get; init; }
+        public string[]? Responses { get; init; }
+        public int ValidResponse { get; init; }
+        public int UserResponse { get; set; }
+        public string? Recap { get; init; }
     }
 }
